d6fd62fef61adb4a6b89f0c7c0e70d418d04f96f
mehdi-baneshi <mehdibaneshi1373@gmail.com>
2023-06-29 14:22:12 +0330

    creation of Infra.IoC project and initial commit of it

---
+++ SimpleRabbit.Infra.Ioc/DependencyContainer.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SimpleRabbit.Core.Domain.Bus;
+using SimpleRabbit.Infra.Bus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleRabbit.Infra.Ioc
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Domain Bus
+            services.AddSingleton<IEventBus, RabbitMQBus>(sp => new RabbitMQBus(sp.GetRequiredService<IServiceScopeFactory>()));
+        }
+    }
+}
